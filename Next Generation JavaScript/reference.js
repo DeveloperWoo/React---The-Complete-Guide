aa5097ae626ddfa2b63e4f3a095c969b25aa4815
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2019-03-17 18:22:46 -0400

    Create reference.js

---
+++ Next Generation JavaScript/reference.js
@@ -0,0 +1,20 @@
+//reference
+const person = {
+  name: 'Hyunju'
+};
+
+//Copy the values inside person using ...(spread operator)
+const firstPerson = {
+  ...person
+};
+console.log(firstPerson); //name: "Hyunju"
+
+//Copy the reference of person
+const secondPerson = person;
+person.name = 'Woo';
+console.log(secondPerson); //name: "Woo"
+
+
+console.log(firstPerson); //name: "Hyunju"
+
+
