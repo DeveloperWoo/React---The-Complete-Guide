eb8abd89fb166022dac5dfb144df098557bb9d98
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2019-03-21 23:31:47 -0400

    Update App.js
    #47. Working with inline styles

--- BaseFeatures&Syntax/App.js
+++ BaseFeatures&Syntax/App.js
@@ -25,19 +25,40 @@ class App extends Component {
     })
   }
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: 'Max', age: 29},
+        {name: event.target.value, age: 25},
+        {name: 'Angela', age: 24}
+      ]
+    })
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I am Hyunju</h1>
         <p>This is really working</p>
-        <button onClick={() => this.switchNameHandler('Hyunju Woo')}>Switch Name</button> 
+        <button 
+          style={style}
+          onClick={() => this.switchNameHandler('Hyunju Woo')}>Switch Name</button> 
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age}/>
         <Person 
           name={this.state.persons[1].name} 
           age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Hyunju Woo!!')}> My hobbie is Reading books</Person>
+          click={this.switchNameHandler.bind(this, 'Hyunju Woo!!')}
+          changed={this.nameChangedHandler}> My hobbie is Reading books</Person>
         <Person 
           name={this.state.persons[2].name} 
           age={this.state.persons[2].age}/>
