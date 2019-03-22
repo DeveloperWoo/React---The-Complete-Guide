e8a272b3aceed5a271bd0c81ca38c909261906bd
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2019-03-21 23:06:40 -0400

    Update App.js
    #44. Passing Method References between components

--- BaseFeatures&Syntax/App.js
+++ BaseFeatures&Syntax/App.js
@@ -1,47 +1,50 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       {name: 'Max', age: 28},
       {name: 'Hyunju', age: 25},
       {name: 'Angela', age: 24}
     ],
     otherState: 'some other value'
-  });
+  }
 
-  const [otherState, setOtherState] = useState('some other value');
-
-  console.log(personsState, otherState);
-  
-  const switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //console.log('Was clicked');
-    //DO NOT DO THIS: personsState.persons[1].name = 'Hyunju Woo';
-    //DO NOT DO THIS: personsState.persons[0].age = 29;
-    setPersonsState({
+    //DO NOT DO THIS: this.state.persons[1].name = 'Hyunju Woo';
+    //DO NOT DO THIS: this.state.persons[0].age = 29;
+    this.setState({
       persons: [
         {name: 'Max', age: 29},
-        {name: 'Hyunju Woo', age: 25},
+        {name: newName, age: 25},
         {name: 'Angela', age: 24}
       ]
     })
-  };
+  }
 
-  return (
-    <div className="App">
-      <h1>Hi, I am Hyunju</h1>
-      <p>This is really working</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}> My hobbie is Reading books</Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
-    </div>
-  );
-  //return(React.createElement('div', {className='App'}, React.createElement('h1', null, 'Hi, I am Hyunju')));
-} 
-  
-    
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I am Hyunju</h1>
+        <p>This is really working</p>
+        <button onClick={this.switchNameHandler.bind(this, 'Hyunju Woo')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Hyunju Woo!!')}> My hobbie is Reading books</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
+      </div>
+    );
+    //return(React.createElement('div', {className='App'}, React.createElement('h1', null, 'Hi, I am Hyunju')));
+  }
+}
 
-export default app;
+export default App;
