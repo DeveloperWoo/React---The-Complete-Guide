81438dad79f8f4905ce2cec4edce0b419ea25a8a
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2019-03-24 18:32:28 -0400

    Update App.js
    #57. Flexible Lists

--- BaseFeatures&Syntax/App.js
+++ BaseFeatures&Syntax/App.js
@@ -13,14 +13,23 @@ class App extends Component {
     showPersons: false
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        {id: 'id1', name: 'Max', age: 29},
-        {id: 'id2', name: event.target.value, age: 25},
-        {id: 'id3', name: 'Angela', age: 24}
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    //const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
   }
 
   deletePErsonHandler = (personIndex) => {
@@ -54,7 +63,8 @@ class App extends Component {
               click = {() => this.deletePErsonHandler(index)}
               name = {person.name} 
               age = {person.age}
-              key = {person.id}/>
+              key = {person.id}
+              changed = {(event) => this.nameChangedHandler(event, person.id)}/>
           })}
         </div>
       );
