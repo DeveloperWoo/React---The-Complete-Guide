d079930c2a883a2c6bf51a030e79224df3059c4f
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2019-03-24 16:47:06 -0400

    Update App.js
    #51. Handling Dynamic Content "The JavaScript Way"

--- BaseFeatures&Syntax/App.js
+++ BaseFeatures&Syntax/App.js
@@ -14,9 +14,9 @@ class App extends Component {
   }
 
   switchNameHandler = (newName) => {
-    // console.log('Was clicked');
-    // DO NOT DO THIS: this.state.persons[1].name = 'Hyunju Woo';
-    // DO NOT DO THIS: this.state.persons[0].age = 29;
+    //console.log('Was clicked');
+    //DO NOT DO THIS: this.state.persons[1].name = 'Hyunju Woo';
+    //DO NOT DO THIS: this.state.persons[0].age = 29;
     this.setState({
       persons: [
         {name: 'Max', age: 29},
@@ -50,6 +50,26 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if(this.state.showPersons){
+      persons = (
+        <div>
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age}/>
+          <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Hyunju Woo!!')}
+            changed={this.nameChangedHandler}> My hobbie is Reading books</Person>
+          <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age}/>
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I am Hyunju's first React App :)</h1>
@@ -57,23 +77,7 @@ class App extends Component {
         <button 
           style={style}
           onClick={this.togglePersonsHandler}>Toggle persons</button> 
-          {
-            this.state.showPersons ?
-              <div>
-                <Person 
-                  name={this.state.persons[0].name} 
-                  age={this.state.persons[0].age}/>
-                <Person 
-                  name={this.state.persons[1].name} 
-                  age={this.state.persons[1].age}
-                  click={this.switchNameHandler.bind(this, 'Hyunju Woo!!')}
-                  changed={this.nameChangedHandler}> My hobbie is Reading books</Person>
-                <Person 
-                  name={this.state.persons[2].name} 
-                  age={this.state.persons[2].age}/>
-              </div> : null
-          }
-        
+        {persons}
       </div>
     );
     //return(React.createElement('div', {className='App'}, React.createElement('h1', null, 'Hi, I am Hyunju')));
@@ -81,4 +85,3 @@ class App extends Component {
 }
 
 export default App;
-
